d63904f1214bf30717e142fb5e497633f3015d77
FredrikFilen <69147419+FredrikFilen@users.noreply.github.com>
2021-12-07 15:51:09 +0100

    added neon styling

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 15;
 
-// Select the elements on the page - canvas, shake button
+// Select the elements on the page
 const canvas = document.querySelector("#etch-a-sketch");
 const ctx = canvas.getContext("2d");
 const shakebutton = document.querySelector(".shake");
 
-// set up our canvas for drawing
+// set up canvas
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 10;
+ctx.globalCompositeOperation = "lighter";
 
 const width = canvas.width;
 const height = canvas.height;
@@ -20,13 +21,14 @@ ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
-// write a draw function
+//drawing
 let hue = Math.random() * 360;
-ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.shadowBlur = 40;
 
 function draw({ key }) {
-  hue += 5;
+  hue += 1;
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.shadowColor = `hsl(${hue}, 100%, 50%)`;
 
   console.log(key);
   ctx.beginPath();
@@ -57,7 +59,7 @@ function draw({ key }) {
   ctx.stroke();
 }
 
-// write a handler for the keys
+// keystrokehandler
 function handleKey(e) {
   if (e.key.includes("Arrow")) {
     e.preventDefault();
@@ -70,7 +72,7 @@ function handleKey(e) {
   }
 }
 
-// clear or shake function
+// clear
 function clearCanvas() {
   canvas.classList.add("shake");
   ctx.clearRect(0, 0, width, height);
@@ -83,6 +85,6 @@ function clearCanvas() {
   );
 }
 
-// listen for arrow keys
+// listeners
 window.addEventListener("keydown", handleKey);
 shakebutton.addEventListener("click", clearCanvas);
